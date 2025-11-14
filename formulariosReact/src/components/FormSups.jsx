c1977978011b7c029c060caeeb047302c98e0f0f
StefaniaSuar <stefaniasuarez1026@gmail.com>
2025-11-14 12:21:49 -0300

    fixes

--- formulariosReact/src/components/FormSups.jsx
+++ formulariosReact/src/components/FormSups.jsx
@@ -1,21 +1,24 @@
     import { useState } from "react";
 
     const FormSups = () => {
+        const [name, setName] = useState ("")
+        const [description, setdescription] = useState ("")
+        const [linkImg, setlinkImg,] = useState ("")
     return (
         <form>
         <div className="mb-3">
             <label htmlFor="name">Name:</label>
-            <input type="text" name="name" id="name" />
+            <input type="text" name="name" id="name" value={name} />
         </div>
 
         <div className="mb-3">
             <label htmlFor="description">Description:</label>
-            <input type="text" name="description" id="description" />
+            <input type="text" name="description" id="description" value={description}/>
         </div>
 
         <div className="mb-3">
             <label htmlFor="linkImg">Img link:</label>
-            <input type="text" name="linkImg" id="linkImg" />
+            <input type="text" name="linkImg" id="linkImg" value={linkImg} />
         </div>
 
         <div>
